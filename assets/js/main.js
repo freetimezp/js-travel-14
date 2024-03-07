686f52b67d8025b4d4b3121d09039300b0a16248
freetimezp <kraftukrnet@ukr.net>
2024-03-07 14:32:02 +0200

    03. js-travel-14. Home section. Slider use Swiper.

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,12 +22,42 @@ function changeHeaderBg() {
 }
 
 
+//thumbnail slider
+const thumbnailsSwiper = new Swiper(".home__thumbnails", {
+    slidesPerView: 3.5,
+    spaceBetween: 20,
+    loop: true,
+    effect: "carousel",
+    allowTouchMove: false,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0: { slidesPerView: 1.5 },
+        800: { slidesPerView: 1.8 },
+        940: { slidesPerView: 2.2 },
+        1000: { slidesPerView: 2.4 },
+        1100: { slidesPerView: 2.5 },
+        1200: { slidesPerView: 2.8 },
+        1300: { slidesPerView: 3.1 },
+        1400: { slidesPerView: 3.5 },
+    }
+});
+
+thumbnailsSwiper.on("slideChange", () => {
+    let realIndex = thumbnailsSwiper.realIndex;
+    let prevRealIndex = thumbnailsSwiper.realIndex;
+
+    document.querySelectorAll(".home__slide")[prevRealIndex].classList.remove('active');
+    document.querySelectorAll(".home__slide")[realIndex].classList.add('active');
+});
 
 
-
-
-
-
+//onload 
+window.addEventListener("load", () => {
+    document.querySelector(".home__thumbnails").classList.add("reveal");
+});
 
 
 
